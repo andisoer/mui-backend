17c9559f1491ba4d0e3705db8df00aabc9b1d3af
Andi Surya <andi502.as@gmail.com>
2024-01-25 11:40:29 +0700

    adjust api home

--- muibackend/muibackend/urls.py
+++ muibackend/muibackend/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('apia/', include('api.v1.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/home/', include('home.urls')),
 ] 
